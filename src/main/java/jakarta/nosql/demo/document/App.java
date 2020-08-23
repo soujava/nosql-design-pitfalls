9dba26532e0ebfaa3808d04b98fd488c73460b4b
otaviojava <otaviopolianasantana@gmail.com>
2020-08-23 07:17:19 -0300

    adds moneta dependency

---
+++ src/main/java/jakarta/nosql/demo/document/App.java
@@ -0,0 +1,20 @@
+package jakarta.nosql.demo.document;
+
+import javax.enterprise.inject.se.SeContainer;
+import javax.enterprise.inject.se.SeContainerInitializer;
+
+public class App {
+
+    public static void main(String[] args) {
+        try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
+
+            Address address = Address.builder()
+                    .withCity("Salvador")
+                    .withCountry("Brazil")
+                    .withPostalCode("40235220")
+                    .build();
+
+
+        }
+    }
+}
