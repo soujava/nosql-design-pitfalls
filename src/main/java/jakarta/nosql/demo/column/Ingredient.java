898a3eef6bb0202bc1cdff82a023eb2f6102a00a
otaviojava <otaviopolianasantana@gmail.com>
2020-08-23 09:13:14 -0300

    creates ingredient model

--- src/main/java/jakarta/nosql/demo/column/Ingredient.java
+++ src/main/java/jakarta/nosql/demo/column/Ingredient.java
@@ -1,2 +1,22 @@
-package jakarta.nosql.demo.column;public class Ingredient {
+package jakarta.nosql.demo.column;
+
+import jakarta.nosql.mapping.Column;
+import jakarta.nosql.mapping.Entity;
+
+@Entity
+class Ingredient {
+
+    @Column
+    private Double quantity;
+
+    @Column
+    private String unit;
+
+    public Ingredient(Double quantity, String unit) {
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+
+    public Ingredient() {
+    }
 }
--- src/main/java/jakarta/nosql/demo/column/Recipe.java
+++ src/main/java/jakarta/nosql/demo/column/Recipe.java
@@ -1,2 +1,22 @@
-package jakarta.nosql.demo.column;public class Recipe {
+package jakarta.nosql.demo.column;
+
+import jakarta.nosql.mapping.Column;
+import jakarta.nosql.mapping.Entity;
+import jakarta.nosql.mapping.Id;
+import org.eclipse.jnosql.artemis.cassandra.column.UDT;
+
+import java.util.Set;
+
+@Entity
+public class Recipe {
+
+    @Id
+    private String name;
+
+    @Column
+    private String city;
+
+    @Column
+    @UDT("ingredient")
+    private Set<Ingredient> ingredients;
 }
