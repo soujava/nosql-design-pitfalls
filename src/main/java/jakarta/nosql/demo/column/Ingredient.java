3078a3e3c6a14187dcaa747e704bac2e8c999716
otaviojava <otaviopolianasantana@gmail.com>
2020-08-23 09:41:27 -0300

    updates in the ingrediente

--- src/main/java/jakarta/nosql/demo/column/Ingredient.java
+++ src/main/java/jakarta/nosql/demo/column/Ingredient.java
@@ -3,20 +3,25 @@ package jakarta.nosql.demo.column;
 import jakarta.nosql.mapping.Column;
 import jakarta.nosql.mapping.Entity;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 
 @Entity
 class Ingredient {
 
     @Column
-    private Double quantity;
+    private String name;
+
+    @Column
+    private BigDecimal quantity;
 
     @Column
     private String unit;
 
-    public Ingredient(Double quantity, String unit) {
-        this.quantity = quantity;
+    public Ingredient(String name, Double quantity, String unit) {
+        this.quantity = BigDecimal.valueOf(quantity);
         this.unit = unit;
+        this.name = name;
     }
 
     public Ingredient() {
--- src/main/java/jakarta/nosql/demo/column/RecipeApp.java
+++ src/main/java/jakarta/nosql/demo/column/RecipeApp.java
@@ -1,2 +1,28 @@
-package jakarta.nosql.demo.column;public class RecipeApp {
+package jakarta.nosql.demo.column;
+
+import jakarta.nosql.column.ColumnQuery;
+import org.eclipse.jnosql.artemis.cassandra.column.CassandraTemplate;
+
+import javax.enterprise.inject.se.SeContainer;
+import javax.enterprise.inject.se.SeContainerInitializer;
+
+public class RecipeApp {
+
+    public static void main(String[] args) {
+        try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
+            CassandraTemplate template = container.select(CassandraTemplate.class).get();
+            Recipe recipe = new Recipe("Bauru", "Bauru");
+            recipe.add(new Ingredient("French bun with crumb", 1D, "unit"));
+            recipe.add(new Ingredient("Cheese", 300D, "grams"));
+            recipe.add(new Ingredient("Roast beef", 500D, "grams"));
+            recipe.add(new Ingredient("Pickled cucumber", 150D, "grams"));
+            recipe.add(new Ingredient("Tomato", 100D, "grams"));
+
+            template.insert(recipe);
+
+            ColumnQuery query = ColumnQuery.select().from("Recipe").where("_id").eq("Bauru").build();
+            template.select(query).forEach(System.out::println);
+
+        }
+    }
 }
