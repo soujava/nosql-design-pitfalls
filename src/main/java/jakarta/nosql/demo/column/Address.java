9ad253351d8d1ba8041d741723a3bc528d7fd135
otaviojava <otaviopolianasantana@gmail.com>
2020-08-23 07:49:37 -0300

    creates column

--- src/main/java/jakarta/nosql/demo/column/Address.java
+++ src/main/java/jakarta/nosql/demo/column/Address.java
@@ -1,2 +1,101 @@
-package jakarta.nosql.demo.column;public class Address {
+package jakarta.nosql.demo.column;
+
+import jakarta.nosql.mapping.Column;
+import jakarta.nosql.mapping.Entity;
+
+import java.util.Objects;
+
+@Entity
+public class Address {
+
+    @Column
+    private String city;
+
+    @Column
+    private String country;
+
+    @Column
+    private String postalCode;
+
+    Address() {
+    }
+
+    Address(String city, String country, String postalCode) {
+        this.city = city;
+        this.country = country;
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) &&
+                Objects.equals(country, address.country) &&
+                Objects.equals(postalCode, address.postalCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, country, postalCode);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                '}';
+    }
+
+    public static AddressBuilder builder() {
+        return new AddressBuilder();
+    }
+
+    public static class AddressBuilder {
+        private String city;
+        private String country;
+        private String postalCode;
+
+        private AddressBuilder() {
+        }
+
+        public Address.AddressBuilder withCity(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public Address.AddressBuilder withCountry(String country) {
+            this.country = country;
+            return this;
+        }
+
+        public Address.AddressBuilder withPostalCode(String postalCode) {
+            this.postalCode = postalCode;
+            return this;
+        }
+
+
+        public Address build() {
+            return new Address(city, country, postalCode);
+        }
+    }
 }
--- src/main/java/jakarta/nosql/demo/column/Contact.java
+++ src/main/java/jakarta/nosql/demo/column/Contact.java
@@ -1,2 +1,25 @@
-package jakarta.nosql.demo.column;public class Contact {
+package jakarta.nosql.demo.column;
+
+import jakarta.nosql.mapping.Column;
+import jakarta.nosql.mapping.Entity;
+import jakarta.nosql.mapping.Id;
+import org.eclipse.jnosql.artemis.cassandra.column.UDT;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+@Entity
+public class Contact {
+
+    @Id
+    private String name;
+
+    @Column
+    private LocalDate birthday;
+
+    @Column
+    private Map<String, String> details;
+
+    @UDT("address")
+    private Address address;
 }
